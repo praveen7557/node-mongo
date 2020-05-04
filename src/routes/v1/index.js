4706de5277e25056e39f34cbb723c190b2542961
praveen <praveen.b@westagilelabs.com>
2020-05-04 12:57:28 +0530

    Authentication with e-mail and Password

---
+++ src/routes/v1/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const authRoute = require("./auth.route");
+const userRoute = require("./user.route");
+
+const router = express.Router();
+
+router.get("/", (req, res) =>
+  res.send({
+    msg: "Success",
+  })
+);
+router.use("/auth", authRoute);
+router.use("/users", userRoute);
+
+module.exports = router;
