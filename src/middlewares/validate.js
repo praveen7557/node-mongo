882d6225061b1140ef903b8acb6737a643f848c9
Praveen Bolla <pbolla@reputation.com>
2021-07-20 12:14:05 +0530

    replaced @hapi/joi with joi

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,4 +1,4 @@
-const Joi = require('@hapi/joi');
+const Joi = require('joi');
 const httpStatus = require('http-status');
 const { pick } = require('lodash');
 const AppError = require('../utils/AppError');
